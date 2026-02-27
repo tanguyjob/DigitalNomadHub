dafb4f6456f9c03d7aeb37ef601e7e69316b168b
Tanguy Verhaegen <tanguy_job@hotmail.fr>
2026-02-27 23:49:22 +0100

    first commit

---
+++ DigitalNomadHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DigitalNomadHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
